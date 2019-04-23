777586495d918bde16e25d37f8f42ad2f085d2d1
persla <you@example.com>
2019-04-23 18:07:44 +0100

    Added router init and play for guess my number

---
+++ router/100_guess.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Create routes using $app programming style.
+ */
+//var_dump(array_keys(get_defined_vars()));
+
+
+
+/**
+ * Init game and redirect to play the game.
+ */
+$app->router->get("guess/init", function () use ($app) {
+    // Init the seiion for the gamestart
+    return $app->response->redirect("guess/play");
+});
+
+
+
+/**
+ * Play the game.
+ */
+$app->router->get("guess/play", function () use ($app) {
+    // echo "Some debugging information";
+    return ["Play the game!!!"];
+});
+
+
+
+/**
+* Showing message Hello World, rendered within the standard page layout.
+ */
+$app->router->get("lek/hello-world-page", function () use ($app) {
+    $title = "Hello World as a page";
+    $data = [
+        "class" => "hello-world",
+        "content" => "Hello World in " . __FILE__,
+    ];
+
+    $app->page->add("anax/v2/article/default", $data);
+
+    return $app->page->render([
+        "title" => $title,
+    ]);
+});
